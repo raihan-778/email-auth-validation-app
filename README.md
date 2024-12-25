e0b88fcb81fe4b4d452364ea813d55f83d2be7f5
raihan-778 <raihanarif778@gmail.com>
2024-12-25 21:52:57 +0600

    readme .md file updated

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+=>Functionality I have to added this project.
+
+1. Form validation using name email password & terms & conditions checked.
+2. User Registration using name email password.
+3. User Login using email password.
+4. Google, Github & twitter login
+5. regular expression implementation for password validation.
+6. Implementing error & success message showing in browser for login error & login success.
+7. Implement error for name, email, password & terms & conditions validation.
--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,9 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
+  const [userRegister, setUserRegister] = useState(null);
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const user = { name, email, password };
+    console.log(name);
+
+    setUserRegister(user);
+    console.log(user);
+  };
   return (
     <div>
-      <h2>Register Component</h2>
+      <div className=" rounded-md m-2 md:max-w-5xl">
+        <form onSubmit={handleRegister} className="p-4">
+          <div className="mx-auto w-3/4">
+            <h2 className="text-2xl mb-2 font-semibold text-amber-400">
+              Complete Your Registration Form
+            </h2>{" "}
+            <input
+              className=" rounded-md p-2 mx-auto mb-2 w-full"
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Enter Your Name"
+              required
+            />
+            <br />
+            <input
+              className="p-2 rounded-md mb-2 w-full"
+              type="email"
+              name="email"
+              id="email"
+            />
+            <br />
+            <input
+              className="p-2 rounded-md mb-2 w-full"
+              type="password"
+              name="password"
+              id="password"
+            />{" "}
+            <br />
+            <button className="btn btn-success" type="submit">
+              Register
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
